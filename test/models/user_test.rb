b363e2ed990f970d5102f67340ff25f3faba235f
Maksim R <cat@mos-it.com>
2020-02-21 18:15:14 +0300

    User model

---
+++ test/models/user_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  test 'should not save User with invalid email' do
+    user = User.new(email: 'sometext')
+    assert_not user.save
+  end
+end
